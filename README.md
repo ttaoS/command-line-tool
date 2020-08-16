e6848eed1d814b4222489bc04020fc09c362b255
tonytao <tony.tao@zip.co>
2020-08-16 16:44:11 +1000

    implement print user logic

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# Rust TCP server
-A TCP socket server, listening for connections.
+# Simple command line tool
+Print user info from a Json file
+
+USAGE:
+    print-users <INPUT> [LIMIT]
+
+ARGS:
+    <INPUT>    Sets the input user file to use
+    <LIMIT>    The number of how many users will be printed. If it is not provided, all users will be printed.
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
 
 ## Build
 
@@ -18,5 +29,11 @@ cargo build
 ## Run
 
 ```bash
-cargo run
+./target/debug/print-users --help 
+./target/debug/print-users /path/data.json  1  
 ```
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+extern crate clap;
+extern crate serde;
+
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Read;
+
+use clap::{App, load_yaml};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    age: u32,
+}
+
+pub trait Display {
+    fn display(&self) -> String;
+}
+
+impl Display for User {
+    fn display(&self) -> String {
+        format!("name: {}, age: {}", self.name, self.age)
+    }
+}
+
+
+fn print<'a>(vals: impl Iterator<Item = &'a User>){
+    for user in vals {
+        println!("{}", user.display());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    let file = matches.value_of("INPUT").unwrap();
+
+    let mut file = File::open(file).unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    // Convert the Point to a JSON string.
+    let users: Vec<User> = serde_json::from_str(&data).unwrap();
+
+    if let Some(string) = matches.value_of("count") {
+        let number = string.parse().unwrap();
+        let itr = users.iter().take(number);
+        print(itr)
+    } else {
+        let itr = users.iter();
+        print(itr)
+    }
 }
